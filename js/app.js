ad182af32be3d7743769537180659acd6810d5fa
Shadi Zaqout <shadizaqout@gmail.com>
2020-06-17 15:48:50 +0300

    Add new Func. AddNewLocation

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
 'use strict';
- var operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var allcookieshops = [];
- var cookieshopTable = document.getElementById('cookies-sold');
-  
- function CookieShop(location, minCust, maxCust, cookiesPerSale) {
+var cookieshopTable = document.getElementById('cookies-sold');
+var storeForm = document.getElementById('store-form');
+
+function CookieShop(location, minCust, maxCust, cookiesPerSale) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.cookiesPerSale = cookiesPerSale;
   this.cookiesSoldPerHr = [];
   allcookieshops.push(this);
+  this.render()
 
 }
- CookieShop.prototype.custPerHr = function () {
+CookieShop.prototype.custPerHr = function () {
   return Math.ceil(Math.random() * ((this.maxCust) - (this.minCust)) + this.minCust);
 };
- 
+
 CookieShop.prototype.cookiesPerHr = function () {
   return Math.round(this.cookiesPerSale * this.custPerHr());
 };
 
-CookieShop.prototype.render = function() { 
+CookieShop.prototype.render = function () {
   var trElement = document.createElement('tr');
   var thElement = document.createElement('th');
   thElement.textContent = this.location;
@@ -66,7 +68,7 @@ function makeHeaderRow() { // Header Row Function
   thElement.textContent = 'Daily Totals';
   trElement.appendChild(thElement);
   theadElement.appendChild(trElement);
-
+  console.log("EEEEE")
   cookieshopTable.appendChild(theadElement);
 }
 
@@ -80,14 +82,13 @@ function totalCookiesPerHour() { // Bottom Totals
 
   for (var i = 0; i < operationHour.length; i++) {
     var totalCookies = 0;
-    for( var j = 0; j < allcookieshops.length; j++) {
+    for (var j = 0; j < allcookieshops.length; j++) {
       totalCookies += allcookieshops[j].cookiesSoldPerHr[i];
       grandTotalCookies += allcookieshops[j].cookiesSoldPerHr[i];
     }
     var tdElement = document.createElement('td');
     tdElement.textContent = totalCookies;
     trElement.appendChild(tdElement);
-
   }
   tdElement = document.createElement('td');
   tdElement.textContent = grandTotalCookies;
@@ -102,12 +103,50 @@ new CookieShop('Paris', 20, 38, 2.3);
 new CookieShop('Lima', 2, 16, 4.6);
 
 function renderallcookieshops() {
-  for(var i in allcookieshops) {
+  for (var i in allcookieshops) {
     allcookieshops[i].render();
   }
 }
+
+
+//Event Handler
+function addNewStore(event) {
+
+  var newStoreName = event.target.name.value;
+  var addMinCustomers = parseInt(event.target.minCustomers.value);
+  var addMaxCustomersseeeeee = parseInt(event.target.maxCustomers.value);
+  var addAvgSale = parseFloat(event.target.avgSale.value);
+
+  event.preventDefault();
+
+  if (!event.target.name.value || !event.target.minCustomers.value || !event.target.maxCustomers.value || !event.target.avgSale.value) {
+    return alert('Oops, you forgot something! Please check your entries and try again.');
+  }
+
+  if (event.target.minCustomers.value < 0 || event.target.maxCustomers.value < 0 || event.target.avgSale.value < 0) {
+    return alert('How are you putting negative values in here? Quit it, you\re not giving away customers and cookies.');
+  }
+
+  if (event.target.minCustomers.value > event.target.maxCustomers.value) {
+    return alert('That\'s not how numbers work, nerd.');
+  }
+  deleterow('cookies-sold')
+  new CookieShop(newStoreName, 2,22, 2.3);
+   
+  
+
+  totalCookiesPerHour();
+}
+
+//Event Listener
+storeForm.addEventListener('submit', addNewStore);
  
- 
- makeHeaderRow();
-renderallcookieshops();
 totalCookiesPerHour();
+
+
+function deleterow(tableID) {
+  var table = document.getElementById(tableID);
+  var rowCount = table.rows.length;
+
+  table.deleteRow(rowCount -1);
+}
